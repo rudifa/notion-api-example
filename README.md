cc499b9ad2d74f11990d7bc4c5a5b0518cabb324
Rudi Farkas <rudi.farkas@gmail.com>
2023-04-21 19:52:30 +0200

    initial commit, get data from Notion works

---
+++ README.md
@@ -0,0 +1,20 @@
+# Notion API access
+
+Demo based on [Getting started with the Notion API JavaScript SDK](https://dev.to/craigaholliday/getting-started-with-the-notion-api-javascript-sdk-c50)
+
+## Setup
+
+Requires a file named `.env` in the project directory, containing the keys from Notion
+
+```
+NOTION_API_KEY=secret_n3w...iZWq # 56 chars token
+NOTION_API_DATABASE=ef7...b44    # 32 chars token
+```
+
+## Run
+
+```
+npm start
+```
+
+Prints the received json to the stdout console.
---
+++ index.js
@@ -0,0 +1,16 @@
+// Require the Client object from the Notion JS SDK
+const {Client} = require('@notionhq/client');
+require('dotenv').config();
+
+const NOTION_API_KEY = process.env.NOTION_API_KEY;
+const NOTION_API_DATABASE = process.env.NOTION_API_DATABASE;
+
+// Create a notion client
+const notion = new Client({auth: NOTION_API_KEY});
+
+(async () => {
+  const databaseId = NOTION_API_DATABASE;
+  // Call `retrieve` on `databases` to retrieve a database object.
+  const response = await notion.databases.retrieve({database_id: databaseId});
+  console.log(response);
+})();
