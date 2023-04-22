6de46bc2b3d4e57fb1b4fba3114bb2c0a28acd26
Rudi Farkas <rudi.farkas@gmail.com>
2023-04-22 15:07:46 +0200

    index.js: add features; README: add detail

--- README.md
+++ README.md
@@ -2,13 +2,28 @@
 
 Demo based on [Getting started with the Notion API JavaScript SDK](https://dev.to/craigaholliday/getting-started-with-the-notion-api-javascript-sdk-c50)
 
+Uses `@notionhq/client`.
+
+## Notion setup
+
+In the target Notion workspace - Settings - Connections - Develop and manage integrations:
+
+- create a new Integration, give it a name
+- copy the `Internal Integration Token` to the local file .env
+
+In the target Notion workspace - target page:
+
+- ... menu - Connections: add a connection to the named Integration
+- Share - Copy link: copy the url link and paste into a text editor
+- extract the first 32-character `UUID` and pste it into the local .env file
+
 ## Setup
 
 Requires a file named `.env` in the project directory, containing the keys from Notion
 
 ```
-NOTION_API_KEY=secret_n3w...iZWq # 56 chars token
-NOTION_API_DATABASE=ef7...b44    # 32 chars token
+NOTION_API_KEY=secret_n3w...iZWq # 56 chars `Internal Integration Token`
+NOTION_API_DATABASE=ef7...b44    # 32 chars `UUID`
 ```
 
 ## Run
--- index.js
+++ index.js
@@ -6,11 +6,41 @@ const NOTION_API_KEY = process.env.NOTION_API_KEY;
 const NOTION_API_DATABASE = process.env.NOTION_API_DATABASE;
 
 // Create a notion client
-const notion = new Client({auth: NOTION_API_KEY, logLevel: LogLevel.DEBUG});
+// const notion = new Client({auth: NOTION_API_KEY, logLevel: LogLevel.DEBUG});
+const notion = new Client({auth: NOTION_API_KEY});
 
+// get a list of databases
 (async () => {
+  // works
   const databaseId = NOTION_API_DATABASE;
   // Call `retrieve` on `databases` to retrieve a database object.
   const response = await notion.databases.retrieve({database_id: databaseId});
-  console.log(response);
+  console.log('\n=== databases response:\n', response);
 })();
+
+// get a list of all pages in a database
+(async () => {
+  const databaseId = NOTION_API_DATABASE;
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  });
+  console.log('\n=== databases query response:\n', response);
+})();
+
+// get a list of blocks
+(async () => {
+  const databaseId = NOTION_API_DATABASE;
+  const response = await notion.blocks.children.list({
+    block_id: databaseId,
+  });
+  console.log('\n=== blocks response:\n', response);
+})();
+
+/*
+// get a list of users
+// message: 'Insufficient permissions for this endpoint.'
+(async () => {
+  const response = await notion.users.list();
+  console.log('\n=== users response:\n', response);
+})();
+*/
